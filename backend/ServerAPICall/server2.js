b0f0aa678eb59f6157f2a6d2adca01f6d54c19f5
Mishmoready <moses@missionreadyhq.com>
2023-08-26 14:35:56 +1200

    added extra server for AI

---
+++ backend/ServerAPICall/server2.js
@@ -0,0 +1,5 @@
+const util = require('util');
+const fs = require('fs');
+const TrainingApi = require("@azure/cognitiveservices-customvision-training");
+const PredictionApi = require("@azure/cognitiveservices-customvision-prediction");
+const msRest = require("@azure/ms-rest-js");
